3dfcb609f793dd65a22d0f4aaec07c7abee1cdf6
SDSamarasinghe <it20605902@my.sliit.lk>
2025-10-12 13:22:19 +0530

    feat: add ScrollToTop component to improve navigation experience

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Services from "./pages/Services";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import { ScrollToTop } from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/services" element={<Services />} />
